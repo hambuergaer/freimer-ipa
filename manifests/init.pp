a6c9d98d58309f9dfa5226b5bc174e2b24d7f765
Frank Reimer <frank.reimer@kuehne-nagel.com>
2016-03-30 16:01:40 +0200

    Frank Reimer: freimer-ipa/metadata.json extended, removed unneeded options from manifests/init.pp, switched to version 0.1.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,6 @@ class ipa ( $ipa_realm       = "EXAMPLE.COM",
                 ) 
 {
 
-include stdlib
 
 # Command to Run to Join to IPA Domain
 $command_string = "/usr/sbin/ipa-client-install --principal $ipa_join_user@$ipa_realm --password $ipa_join_pw --domain $ipa_domain --server $ipa_server_1 --server $ipa_server_2 --mkhomedir --unattended --force -N"
@@ -21,21 +20,5 @@ exec { "ipa_installer":
 	unless      => '/usr/sbin/ipa-client-install --unattended 2>&1 | /bin/grep -q "already configured"',
 	require     => Package["ipa-client"],
 	}
-
-#if $operatingsystem == "RedHat" and $operatingsystemmajrelease == 6 {
-#	file_line { 'dns_lookup_realm':
-#		path    => '/etc/krb5.conf',
-#		line    => "  dns_lookup_realm = true",
-#		match   => '[\s]dns_lookup_realm',
-#		ensure  => present,
-#		}
-#
-#	file_line { 'dns_lookup_kdc':
-#		path    => '/etc/krb5.conf',
-#		line    => "  dns_lookup_kdc = true",
-#		match   => '[\s]dns_lookup_kdc',
-#		ensure  => present,
-#		}
-#}
 
 }
